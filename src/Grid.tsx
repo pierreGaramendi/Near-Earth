d01468bdc323456a7c6ee1220e41223b509a7c3f
Pierre Garamendi <pierre.garamendi@gmail.com>
2024-10-01 08:00:12 -0500

    filter by column was added

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -5,27 +5,23 @@ import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 
 const columnDefs: ColDef[] = [
-  { field: "designation", headerName: "Designation" },
-  { field: "discovery_date", headerName: "Discovery Date" },
-  { field: "h_mag", headerName: "H (mag)" },
-  { field: "moid_au", headerName: "MOID (au)" },
-  { field: "q_au_1", headerName: "q (au)" },
-  { field: "q_au_2", headerName: "Q (au)" },
-  { field: "period_yr", headerName: "Period (yr)" },
-  { field: "i_deg", headerName: "Inclination (deg)" },
-  { field: "pha", headerName: "Potentially Hazardous" },
-  { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true, },
+  { field: "designation", headerName: "Designation", sortable: true },
+  { field: "discovery_date", headerName: "Discovery Date", sortable: true },
+  { field: "h_mag", headerName: "H (mag)", sortable: true },
+  { field: "moid_au", headerName: "MOID (au)", sortable: true },
+  { field: "q_au_1", headerName: "q (au)", sortable: true },
+  { field: "q_au_2", headerName: "Q (au)", sortable: true },
+  { field: "period_yr", headerName: "Period (yr)", sortable: true },
+  { field: "i_deg", headerName: "Inclination (deg)", sortable: true },
+  { field: "pha", headerName: "Potentially Hazardous", sortable: true },
+  { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true, sortable: true },
 ];
 
 const NeoGrid = (): JSX.Element => {
   return (
     <div className="ag-theme-alpine" style={{ height: 900, width: 1920 }}>
       <h1>Near-Earth Object Overview</h1>
-      <AgGridReact
-        rowData={data}
-        columnDefs={columnDefs}
-        rowGroupPanelShow={'always'}
-      />
+      <AgGridReact rowData={data} columnDefs={columnDefs} rowGroupPanelShow={"always"} rowSelection={"multiple"} />
     </div>
   );
 };
